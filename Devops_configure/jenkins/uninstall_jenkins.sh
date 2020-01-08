521f48568bb1a21810ee4dc24a82e56f21534d4e
zaid84120 <44051286+zaid84120@users.noreply.github.com>
2020-01-08 16:39:37 +0530

    updated

--- Devops_configure/jenkins/uninstall_jenkins.sh
+++ Devops_configure/jenkins/uninstall_jenkins.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 sudo service jenkins stop
-sudo yum clean all
-sudo yum -y remove jenkins
-sudo rm -rf /var/cache/jenkins
-sudo rm -rf /var/lib/jenkins/
+apt-get remove --purge jenkins
